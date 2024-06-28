1a8375e3164ed1beb7083db91d9ec9445fc156a0
haizhixin2021 <96433012+haizhixin2021@users.noreply.github.com>
2024-06-29 00:53:47 +0800

    Update startcode-server.sh

--- images/jupyter-codeserver/startcode-server.sh
+++ images/jupyter-codeserver/startcode-server.sh
@@ -2,7 +2,7 @@
 
 # 启动JupyterLab
 #jupyter lab --ip=0.0.0.0 --port=8888 --no-browser &
-start-notebook.py
+start-notebook.py &
 
 # 启动Code-Server
 code-server --bind-addr 0.0.0.0:8080 --auth password
